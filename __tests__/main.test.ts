ae23e3d354089cbd7baabbdfabef4f77f8b305a7
Prabhat Pal <prabhatpal23@gmail.com>
2020-07-10 20:33:29 +0530

    update main.test.ts

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,23 +1,124 @@
-//we want to test if the run function is calling the @actions/core.setOutput() correctly or not
-//this is a unit test for just the run() function, therefore we should create a fake context in which a fake version of the @actions/core.setOutput() is created in the testbench where run() is tested in isolation
-//to do that, we can use the jest comes with it's own built-in mocking library
-
-//start by importing '@actions/core' so that we know what we want to mock
 import * as core from '@actions/core'
-import { run } from '../src/main'
+import * as event from '../src/event'
+import * as version from '../src/version'
+import * as git from '../src/git'
+import * as github from '../src/github'
+import {when} from 'jest-when'
+import {run} from '../src/main'
 
-//mock the core package
 jest.mock('@actions/core')
+jest.mock('../src/event')
+jest.mock('../src/version')
+jest.mock('../src/git')
+jest.mock('../src/github')
 
-describe('When running the action', ()=>{
-  //create a fakeSetOutput function as the mocked version of the core.setOutput() from the mocked @actions/core
-  //do that by casting the required function to the mocked function of t-type as follows
+describe('When running the action with a created version tag event', () => {
+  const fakeGetCreatedTag = event.getCreatedTag as jest.MockedFunction<typeof event.getCreatedTag>
+  const fakeIsSemVer = version.isSemVer as jest.MockedFunction<typeof version.isSemVer>
+  const fakeGetChanges = git.getChangesIntroducedByTag as jest.MockedFunction<typeof git.getChangesIntroducedByTag>
+  const fakeGetInput = core.getInput as jest.MockedFunction<typeof core.getInput>
+  const fakeCreateRelease = github.createReleaseDraft as jest.MockedFunction<typeof github.createReleaseDraft>
   const fakeSetOutput = core.setOutput as jest.MockedFunction<typeof core.setOutput>
 
-  test('it should set the release-url output parameter', async()=>{
-    await run()
-    //expect to have fakeSetOutput being aclled with release-url parameter, and any other parameter (because we have hardcoded that to dummy url) 
-    expect(fakeSetOutput).toHaveBeenCalledWith('release-url',expect.anything())
+  beforeAll(() => {
+    fakeGetCreatedTag.mockReturnValue('v1.0.0')
+    when(fakeIsSemVer)
+      .calledWith('v1.0.0')
+      .mockReturnValue(true)
+    when(fakeGetChanges)
+      .calledWith('v1.0.0')
+      .mockReturnValue(Promise.resolve('changelog'))
+    when(fakeGetInput)
+      .calledWith('repo-token')
+      .mockReturnValue('the-token')
+    when(fakeCreateRelease)
+      .calledWith('v1.0.0', 'the-token', 'changelog')
+      .mockReturnValue(Promise.resolve('http://release'))
   })
 
+  beforeEach(async () => {
+    await run()
+  })
+
+  test('it should create a release draft for the created version tag', () => {
+    expect(fakeCreateRelease).toHaveBeenCalledWith('v1.0.0', 'the-token', 'changelog')
+  })
+
+  test('it should set the created release URL as output parameter', () => {
+    expect(fakeSetOutput).toHaveBeenCalledWith('release-url', 'http://release')
+  })
+})
+
+describe('When running the action with another kind of event', () => {
+  const fakeGetCreatedTag = event.getCreatedTag as jest.MockedFunction<typeof event.getCreatedTag>
+  const fakeCreateRelease = github.createReleaseDraft as jest.MockedFunction<typeof github.createReleaseDraft>
+  const fakeSetOutput = core.setOutput as jest.MockedFunction<typeof core.setOutput>
+
+  beforeAll(() => {
+    fakeGetCreatedTag.mockReturnValue(null)
+  })
+
+  beforeEach(async () => {
+    await run()
+  })
+
+  test('it should not create any release draft', () => {
+    expect(fakeCreateRelease).not.toHaveBeenCalled()
+  })
+
+  test('it should set the release-url output parameter to an empty string', () => {
+    expect(fakeSetOutput).toHaveBeenCalledWith('release-url', '')
+  })
+})
+
+describe('When running the action with a tag created event that does not conform to SemVer', () => {
+  const fakeGetCreatedTag = event.getCreatedTag as jest.MockedFunction<typeof event.getCreatedTag>
+  const fakeIsSemVer = version.isSemVer as jest.MockedFunction<typeof version.isSemVer>
+  const fakeCreateRelease = github.createReleaseDraft as jest.MockedFunction<typeof github.createReleaseDraft>
+  const fakeSetOutput = core.setOutput as jest.MockedFunction<typeof core.setOutput>
+
+  beforeAll(() => {
+    fakeGetCreatedTag.mockReturnValue('1.0')
+    when(fakeIsSemVer)
+      .calledWith('1.0')
+      .mockReturnValue(false)
+  })
+
+  beforeEach(async () => {
+    await run()
+  })
+
+  test('it should not create any release draft', () => {
+    expect(fakeCreateRelease).not.toHaveBeenCalled()
+  })
+
+  test('it should set the release-url output parameter to an empty string', () => {
+    expect(fakeSetOutput).toHaveBeenCalledWith('release-url', '')
+  })
+})
+
+describe('When creating a release draft fails', () => {
+  const fakeIsSemVer = version.isSemVer as jest.MockedFunction<typeof version.isSemVer>
+  const fakeCreateRelease = github.createReleaseDraft as jest.MockedFunction<typeof github.createReleaseDraft>
+  const fakeSetFailed = core.setFailed as jest.MockedFunction<typeof core.setFailed>
+  const fakeSetOutput = core.setOutput as jest.MockedFunction<typeof core.setOutput>
+
+  beforeAll(() => {
+    fakeIsSemVer.mockReturnValue(true)
+    fakeCreateRelease.mockImplementation(() => {
+      throw new Error('Failed to create the release')
+    })
+  })
+
+  beforeEach(async () => {
+    await run()
+  })
+
+  test('it should set the action status to failed with the error message', () => {
+    expect(fakeSetFailed).toHaveBeenCalledWith('Failed to create the release')
+  })
+
+  test('it should not set the release-url output parameter', () => {
+    expect(fakeSetOutput).not.toHaveBeenCalledWith('release-output', expect.anything())
+  })
 })
